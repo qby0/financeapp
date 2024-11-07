8ee4344305fcd1661f52664f74deb6d33135ab5f
Murlay <166720850+Murlay@users.noreply.github.com>
2024-11-08 00:01:06 +0100

    Update CostsFilter.js

--- src/components/NewCost/CostsFilter.js
+++ src/components/NewCost/CostsFilter.js
@@ -1,8 +1,13 @@
-import "./CostsFilter.css";
+import React from 'react';
+import './CostsFilter.css';
 
 const CostsFilter = (props) => {
   const yearChangeHandler = (event) => {
     props.onChangeYear(event.target.value);
+  };
+
+  const monthChangeHandler = (event) => {
+    props.onChangeMonth(event.target.value);
   };
 
   return (
@@ -15,6 +20,24 @@ const CostsFilter = (props) => {
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
+        </select>
+      </div>
+      <div className="costs-filter__control">
+        <label>Filter by month</label>
+        <select value={props.month} onChange={monthChangeHandler}>
+          <option value="">All</option>
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+          <option value="06">June</option>
+          <option value="07">July</option>
+          <option value="08">August</option>
+          <option value="09">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
         </select>
       </div>
     </div>
