7b35d9b448a113fe60405131c2ac769adbd30bf3
ant103-72 <anton.osiievskyi@student.tuke.sk>
2024-11-19 21:02:39 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import AchievementsModal from './AchievementsModal'; // Импорт нового модального окна
 
 const Navbar = ({ user, onSignUpClick, onLoginClick, onLogoutClick }) => {
+  const [isAchievementsOpen, setIsAchievementsOpen] = useState(false);
+
+  const handleAchievementsClick = () => {
+    setIsAchievementsOpen(true);
+  };
+
+  const handleCloseAchievements = () => {
+    setIsAchievementsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-                <div>
+        <div>
           <img src="/logo.png" alt="Logo" className="logo-image" />
         </div>
       </div>
@@ -17,7 +28,9 @@ const Navbar = ({ user, onSignUpClick, onLoginClick, onLogoutClick }) => {
       <div className="navbar-buttons">
         {user ? (
           <>
-            <span className="navbar-nickname">Welcome: {user.nickname || user.email}</span>
+            <div className="navbar-nickname" onClick={handleAchievementsClick}>
+              {user.nickname || user.email}
+            </div>
             <button className="logout-btn" onClick={onLogoutClick}>Logout</button>
           </>
         ) : (
@@ -27,6 +40,7 @@ const Navbar = ({ user, onSignUpClick, onLoginClick, onLogoutClick }) => {
           </>
         )}
       </div>
+      <AchievementsModal isOpen={isAchievementsOpen} onClose={handleCloseAchievements} />
     </nav>
   );
 };
